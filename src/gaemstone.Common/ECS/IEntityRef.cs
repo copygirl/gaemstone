11b7563c80800a926c853ed247c55c3f35154ba6
copygirl <copygirl@mcft.net>
2020-12-15 10:43:34 +0100

    Add IEntityRef.IsAlive and .Destroy()

--- src/gaemstone.Common/ECS/IEntityRef.cs
+++ src/gaemstone.Common/ECS/IEntityRef.cs
@@ -5,11 +5,17 @@ namespace gaemstone.Common.ECS
 		Universe Universe { get; }
 		Entity Entity { get; }
 
+		bool IsAlive
+			=> Universe.Entities.IsAlive(Entity);
+
 		T Get<T>()
 			=> Universe.Get<T>(Entity);
 		bool Has<T>(Entity entity)
 			=> Universe.Has<T>(Entity);
 		void Set<T>(Entity entity, T value)
 			=> Universe.Set<T>(Entity, value);
+
+		void Destroy()
+			=> Universe.Entities.Destroy(Entity);
 	}
 }
